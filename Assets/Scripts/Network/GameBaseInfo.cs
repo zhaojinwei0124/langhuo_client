40ca2ee890749d1fe7679a44555d8a9fd30cb642
huailiang.peng <huailiang.peng@huailiangpeng.local>
2016-01-01 12:05:41 +0800

    gameinfo

--- Assets/Scripts/Network/GameBaseInfo.cs
+++ Assets/Scripts/Network/GameBaseInfo.cs
@@ -11,6 +11,23 @@ namespace Network
 
         public string encrypt{ get; set; }
 
+        public string city{ get; set; }
+
+        public string distric{ get; set; }
+
+        public struct BuyNode
+        {
+            public int id;
+            public int cnt;
+
+            public BuyNode (int _id,int _cnt)
+            {
+                id=_id;
+                cnt=_cnt;
+            }
+        };
+
+        public List<BuyNode> buy_list=new List<BuyNode>();
     }
 
 }
